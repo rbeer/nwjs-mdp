3ab01a4ac6760cb2c7bd696886b5a9c364919a42
Raphael Beer <raphael.beer@protonmail.com>
2016-12-28 22:08:21 +0100

    Change: squash into one object

--- static-page/mdp.js
+++ static-page/mdp.js
@@ -2,36 +2,57 @@
 
 (() => {
 
-  let nAgt = window.navigator.userAgent;
-  let titleByOs = {
-    linux: '/opt/nwjs-mdp/',
-    win: 'C:\\nwjs-mdp\\',
-    macos: '/nwjs.apps/',
-    android: './',
-    ios: './',
-    unknown: './'
-  }
-  let osRX = {
-    linux: /(?:Linux|X11)/,
-    win: /(?:Win16|Windows )/,
-    macos: /(?:MacPPC|MacIntel|Mac_PowerPC|Macintosh|Mac OS X)/,
-    android: /Android/,
-    ios: /(?:iPhone|iPad|iPod)/
-  };
-  let mdP = {};
 
-  mdP.probeForOS = () => {
-    for (let osKey in osRX) {
-      if (osRX[osKey].test(nAgt)) {
-        return osKey;
+  let mdP = {
+    agent: window.navigator.userAgent,
+    os: {
+      linux: {
+        rx: /(?:Linux|X11)/,
+        prefix: '/opt/nwjs-mdp/'
+      },
+      win: {
+        rx: /(?:Win16|Windows )/,
+        prefix: 'C:\\nwjs-mdp\\'
+      },
+      macos: {
+        rx: /(?:MacPPC|MacIntel|Mac_PowerPC|Macintosh)/,
+        prefix: '/opt/nwjs-mdp/'
+      },
+      macosx: {
+        rx: /Mac OS X/,
+        prefix: '/nwjs.app/Contents/Resources/nwjs-mdp.nw/'
+      },
+      android: {
+        rx:  /Android/,
+        prefix: './'
+      },
+      ios: {
+        rx: /(?:iPhone|iPad|iPod)/,
+        prefix: './'
+      },
+      unknown: {
+        prefix: './'
       }
     }
-    return 'unknown';
+  };
+
+  mdP.probeForOS = () => {
+    let os = mdP.os;
+    for (let osKey in os) {
+      if (os[osKey].rx.test(mdP.agent)) {
+        return os[osKey];
+      }
+    }
+    return os.unknown;
   };
 
   mdP.setTitleFileName = () => {
     let $fnElement = $('.filename');
-    $fnElement.text(titleByOs[mdP.probeForOS()] + 'README.md');
+    $fnElement.text(mdP.probeForOS().prefix + 'README.md');
+  };
+
+  mdP.toolbarClickHandler = (evt) => {
+
   };
 
   window.mdP = mdP;
