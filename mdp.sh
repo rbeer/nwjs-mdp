487fbbbbed84af100db1ec8847d13be6ac77a939
Raphael Beer <raphael.beer@protonmail.com>
2017-01-01 21:17:46 +0100

    Add: run-script mdp.sh

---
+++ mdp.sh
@@ -0,0 +1,34 @@
+function print_help {
+  echo ".mdP - 0.0.9"
+  echo "Github flavored markdown live preview."
+  echo -e "Depends: 'nwjs and packages, see package.json'\n"
+  if [ -n "$1" ]; then
+    echo -e "\e[31m--- Invalid option: -$1 ---\e[0m\n"
+  fi
+  echo "Usage: ./mdp.sh -f <filename.md> [-w <true>]"
+  echo "  -f filename  | Path to .md file to preview. Default: ./README.md"
+  echo -e "  -w           | Whether to auto-attach the file-watcher. Default: true \n"
+  exit 1
+}
+
+while getopts ":w:hf:" opt; do
+  case $opt in
+    \?)
+      print_help $OPTARG
+      ;;
+    :)
+      print_help $OPTARG
+      ;;
+    h)
+      print_help
+      ;;
+    f)
+      INPUTPATH=$OPTARG
+      ;;
+    w)
+      FILEWATCHER=$OPTARG
+      ;;
+  esac
+done
+
+nw . -f $INPUTPATH -w $FILEWATCHER
